ba32b00eb8990311676e752c2303e54c10b37588
Flavia Banchieri <109521162+flaviabanchieri@users.noreply.github.com>
2023-08-10 17:32:53 -0400

    Adicionar arquivos de projeto.

---
+++ Model/Clientes.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PsiicApi.Model
+{
+	public class Cliente
+	{
+		[Key]
+		public int Id { get; private set; }
+		public string Nome { get; private set; }
+		public string Contato { get; private set; }
+		public string Observacoes { get; private set; }
+		public bool PacoteSemanal { get; private set; }
+		public decimal ValorConsulta { get; private set; }
+		public decimal SaldoDevedor { get; private set; }
+		public long Telefone { get; private set; }
+		public int EnderecoId { get; private set; }
+		public Endereco Endereco { get; private set; }
+
+		public Cliente(int id, string nome, string contato, string observacoes, bool pacoteSemanal, decimal valorConsulta, decimal saldoDevedor, long telefone, int enderecoId, Endereco endereco)
+		{
+			Id = id;
+			Nome = nome;
+			Contato = contato;
+			Observacoes = observacoes;
+			PacoteSemanal = pacoteSemanal;
+			ValorConsulta = valorConsulta;
+			SaldoDevedor = saldoDevedor;
+			Telefone = telefone;
+			Endereco = endereco;
+		}
+	}
+
+}
---
+++ Model/Endereco.cs
@@ -0,0 +1,13 @@
+﻿namespace PsiicApi.Model
+{
+	public class Endereco
+	{
+		public string Cep { get; set; }
+		public string Logradouro { get; set; }
+		public string Numero { get; set; }
+		public string Bairro { get; set; }
+		public string Cidade { get; set; }
+		public string Estado { get; set; }
+		public string Pais { get; set; }
+	}
+}
